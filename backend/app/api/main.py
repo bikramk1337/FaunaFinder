6b3b3eecfc6d9f3ca6f4dd61e4568cd69f940e3e
Bikram Karki <bikramk1337@gmail.com>
2024-03-25 00:46:47 +1100

    feat(file-upload): upload files to s3 bucket
    - Setup s3 client using boto3
    - Add endpoint to upload image to s3 bucket

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, signup, users, fauna
+from app.api.routes import login, signup, users, fauna, aws
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(signup.router, tags=["signup"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(fauna.router, prefix="/fauna", tags=["fauna"])
+api_router.include_router(aws.router, prefix="/aws", tags=["aes s3"])
