4fecf48208072b20889dcdfab4cd5b40b5addd6c
resha <resha.adhikari1279@gmail.com>
2024-03-25 21:12:20 +1100

    feat(dashboard): added empty dashboard items

--- ff-web/src/Pages/Dashboard/Dashboard.tsx
+++ ff-web/src/Pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,35 @@
+import { Box, Card, Grid } from "@mui/material";
 import React from "react";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
-  return <div>Dashboard</div>;
+  return (
+    <Box>
+      <Grid container spacing={4}>
+        <Grid item xs={8}>
+          <Card variant="outlined" sx={{ height: 300, textAlign: "center" }}>
+            dashboard item 1
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card variant="outlined" sx={{ height: 300, textAlign: "center" }}>
+            dashboard item 2
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card variant="outlined" sx={{ height: 300, textAlign: "center" }}>
+            dashboard item 3
+          </Card>
+        </Grid>
+        <Grid item xs={8}>
+          <Card variant="outlined" sx={{ height: 300, textAlign: "center" }}>
+            dashboard item 4
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Dashboard;
