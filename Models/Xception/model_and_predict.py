4401bb2ccf4d5303121abc4f67cddf496f2f3d36
Prabhat <pmadhikari@gmail.com>
2024-05-15 03:35:53 +1000

    Added Xception Model Created directory Models with Subdirectory Xception containing necessary files to create the model and the script contains the function that will return the predicted class name for the image file provided.

---
+++ Models/Xception/model_and_predict.py
@@ -0,0 +1,23 @@
+from tensorflow.keras.preprocessing import image
+import keras
+import json
+import numpy as np
+import os
+
+# array_data simply contains the list of classes
+with open('array_data.json', 'r') as f:
+    arr_list_reconstructed = json.load(f)
+
+reconstructed_model = keras.models.load_model("my_model.keras")
+
+def predict(test_img_file):
+  img = image.load_img(test_img_file, target_size=(299, 299))  
+  img = image.img_to_array(img)
+  img = np.expand_dims(img, axis=0)  # Add batch dimension
+  # img = preprocess_input(img)
+  pred = reconstructed_model.predict(img)
+  output_class = arr_list_reconstructed[np.argmax(pred)]
+  return output_class
+
+# result = predict('/content/ImageDS/Ibis/Image_106.jpg')
+# print(result)
