8ec1fd960f82f1ea4cf7fd235b873d2088d23a2f
ff <bikramk1337@gmail.com>
2024-05-16 04:51:46 +0000

    fix(deploy): seperate infrastructre container stack and update build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 
 # Check if an argument is provided
 if [ -z "$1" ]; then
-  echo "Please provide an argument: dev or prod"
+  echo "Please provide an argument: dev, prod, or infra"
   exit 1
 fi
 
@@ -14,8 +14,10 @@ if [ "$1" = "dev" ]; then
   COMPOSE_FILE="docker-compose.dev.yml"
 elif [ "$1" = "prod" ]; then
   COMPOSE_FILE="docker-compose.prod.yml"
+elif [ "$1" = "infra" ]; then
+  COMPOSE_FILE="docker-compose.infra.yml"
 else
-  echo "Invalid argument. Please use 'dev' or 'prod'."
+  echo "Invalid argument. Please use 'dev', 'prod', or 'infra'."
   exit 1
 fi
 
@@ -23,11 +25,14 @@ fi
 docker compose -f $COMPOSE_FILE build
 docker compose -f $COMPOSE_FILE up -d
 
-# Hack to wait for postgres container to be up before running alembic migrations
-sleep 5;
+# Run migrations and create initial data only for dev and prod
+if [ "$1" = "dev" ] || [ "$1" = "prod" ]; then
+  # Hack to wait for postgres container to be up before running alembic migrations
+  sleep 5;
+  
+  # Run migrations
+  docker exec ff-backend alembic upgrade head
 
-# Run migrations
-docker exec ff-backend alembic upgrade head
-
-# Create initial data
-docker exec ff-backend python3 app/seed.py
+  # Create initial data
+  docker exec ff-backend python3 app/seed.py
+fi
