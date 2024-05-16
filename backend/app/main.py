d1b37f32d5cddf4abf48bc11724b34d5ea8960bb
ff <bikramk1337@gmail.com>
2024-05-16 02:32:19 +0000

    ref(cors): allow all origin for now

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,9 +14,10 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[
-            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
-        ],
+        # allow_origins=[
+        #     str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+        # ],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
